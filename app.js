fde6ffaeb4c5ce367a73593c0c7dfc06d7ebab8c
marcos.florencio <dstepbw@gmail.com>
2016-07-12 18:35:08 -0300

    create function to get value of input

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
- $(document).ready(function() {
+  $(document).ready(function() {
 
-   $('#format').click(function() {
-     var query = ($('#query').val()).replace(/"/g, '').replace(/[+]/g, ''),
-       url = 'http://sqlformat.org/api/v1/format',
-       method = 'POST';
-     $.ajax({
-       url: url,
-       type: method,
-       dataType: 'json',
-       crossDomain: true,
-       data: {
-         sql: query,
-         reindent: 1,
-         keyword_case: 'upper'
-       },
-     }).done(function(response) {
-       $('#query').val(response.result);
-     });
-   });
+    $('#format').click(function() {
+      var query = getValueOfQuery().replace(/[+]/g, ''),
+        url = 'http://sqlformat.org/api/v1/format',
+        method = 'POST';
+      $.ajax({
+        url: url,
+        type: method,
+        dataType: 'json',
+        crossDomain: true,
+        data: {
+          sql: query,
+          reindent: 1,
+          keyword_case: 'upper'
+        },
+      }).done(function(response) {
+        $('#query').val(response.result);
+      });
+    });
 
-   $('#toString').click(function() {
-     var query = ($('#query').val()).replace(/"/g, '').replace(/[+]/g, '').split('\n'),
-       newQuery = '';
+    $('#toString').click(function() {
+      var query = getValueOfQuery().split('\n'),
+        newQuery = '';
 
-     query.map(function(line) {
-       newQuery += '+ " ' + line + '" \n';
-     });
+      query.map(function(line) {
+        newQuery += '+ " ' + line + '" \n';
+      });
 
-     $('#query').val(newQuery);
-   });
+      $('#query').val(newQuery);
+    });
 
- });
+    function getValueOfQuery() {
+      return ($('#query').val()).replace(/"/g, '').replace(/[+]/g, '');
+    };
+
+  });
